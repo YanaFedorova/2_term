656955f615c7095db75b9ffc6ad5f448f738ef31
YanaFedorova <avorodefanay@gmail.com>
2014-06-27 11:05:40 +0400

    Update 1task.cpp

--- 1task.cpp
+++ 1task.cpp
@@ -4,77 +4,77 @@
 #include "stack"
 
 using namespace std;
-class S
+class ST
 {
-public:
-stack<int> original;
-stack<int> auxiliary;
-int i;
-int m;
-void set(int x)
-{
-for(i=0;i<x;i++) 
-{
-original.push(rand()%100);
-cout « original.top() « " ";
-}
-
-cout «"\n";
-};
-void sort(int x)
-{
-for(i=0;i<x;i++)
-{
-m=original.top();
-original.pop();
-while(!original.empty())
-{
-if(m<=original.top())
-{
-auxiliary.push(m);
-m=original.top();
-original.pop();
-}
-else
-{
-auxiliary.push(original.top());
-original.pop();
-}
-}
-original.push(m);
-while(!auxiliary.empty())
-{
-original.push(auxiliary.top());
-auxiliary.pop();
-}
-}
-while(!original.empty()) 
-{
-cout « original.top() « " ";
-original.pop();
-}
-};
-};
-int main()
-{
-setlocale(LC_ALL,"Russian");
-S obj;
-//cout«"Ââåäèòå ÷èñëî ýëåìåíòîâ â ñòåêå: ";
-int x;
-cin»x;
-if(x<=0)
-{
-//cout«"÷èñëî äîëæíî áûòü áîëüøå íóëÿ \n";
-}
-else
-{
-cout«"\n";
-//cout «"Ïåðâûé ñòåê: ";
-obj.set(x);
-//cout « "Ðåçóëüòàò ñîðòèðîâêè ñòåêà: ";
-obj.sort(x);
-}
-getch();
-return 0;
-
-}
+	public:
+	stack<int> mainarray;
+	stack<int> auxiliary;
+	int i;
+	int m;
+	void set( int x )
+	{
+		for( i = 0; i < x; i++ ) 
+		{
+			mainarray.push(rand()%100);
+			cout << mainarray.top() << " ";
+		}
+		
+		cout << endl;
+	};
+	void sort( int x )
+	{
+		for( i = 0; i < x; i++)
+		{
+			m=mainarray.top();
+			mainarray.pop();
+			while( !mainarray.empty() )
+			{
+			if( m <= mainarray.top() )
+			{
+			auxiliary.push( m );
+			m=mainarray.top();
+			mainarray.pop();
+			}
+			else
+			{
+			auxiliary.push( mainarray.top() );
+			mainarray.pop();
+			}
+			}
+			mainarray.push( m );
+			while( !auxiliary.empty() )
+			{
+			mainarray.push( auxiliary.top() );
+			auxiliary.pop();
+			}
+		}
+		while( !mainarray.empty() ) 
+		{
+		cout << mainarray.top() << " ";
+		mainarray.pop();
+		}
+	};
+	};
+	int main()
+	{
+	setlocale( LC_ALL, "Russian" );
+	ST obj;
+	cout << "Введите значение х (количество элементов в первом массиве): " << endl;
+	int x;
+	cin >> x;
+	if(x <= 0)
+	{
+	cout << "х должен быть положительным числом! " << endl;
+	}
+	else
+	{
+	cout << endl;
+	cout << "Начальный массив: " << endl;
+	obj.set(x);
+	cout << "Массив после сортировки: " << endl;
+	obj.sort(x);
+	}
+	getch();
+	return 0;
+	
+	}
